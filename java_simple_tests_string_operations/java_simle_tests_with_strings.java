ff90246e6417c0552f342598448686231dc65863
Valeriy <fokin0210@gmail.com>
2014-12-17 01:23:23 +0100

    Update java_simle_tests_with_strings.java
    add string reverse

--- java_simple_tests_string_operations/java_simle_tests_with_strings.java
+++ java_simple_tests_string_operations/java_simle_tests_with_strings.java
@@ -5,6 +5,7 @@
         System.out.println("result is:" + result);
     }
     
+    // "apple" and "plaep" return true
     private static boolean acronims1(String arg1, String arg2){
         if(arg1 == null || arg2 == null)
             return false;
@@ -30,6 +31,48 @@
         }
         
         return str2.length() == 0 && str1.length() == 0;
+    }
+    
+    // "apple" return "elppa"
+    private static String strrev(String arg1){
+        if(arg1 == null)
+            return "undefined string";
+        
+        if(arg1.length() < 0)
+            return "undefined string";
+        
+        StringBuilder str1 = new StringBuilder(arg1);
+        
+        for(int ix = 0; ix < str1.length() / 2; ++ix){
+            char tmp = str1.charAt(ix);
+            str1.setCharAt(ix, str1.charAt(str1.length() - ix - 1));
+            str1.setCharAt(str1.length() - ix - 1, tmp);
+        }
+        
+        return str1.toString();
+    }
+    
+    private static String strrev1(String arg1){
+        if(arg1 == null)
+            return "undefined string";
+
+        return new StringBuilder(arg1).reverse().toString();
+    }
+    
+    private static String strrev2(String arg1){
+        if(arg1 == null)
+            return "undefined string";
+        
+        char[] rev = arg1.toCharArray();
+        for(int ix = 0; ix < rev.length / 2; ++ix){
+            int in = rev.length - ix - 1;
+            char f = rev[ix];
+            char l = rev[in];
+            rev[ix] = l;
+            rev[in] = f;
+        }
+        
+        return new String(rev);
     }
     
   }
