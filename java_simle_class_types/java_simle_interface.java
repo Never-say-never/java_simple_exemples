511b4745c7b4ee5310838c6296357e2deec6e557
Valeriy <fokin0210@gmail.com>
2015-02-03 17:16:02 +0100

    Update java_simle_interface.java

--- java_simle_class_types/java_simle_interface.java
+++ java_simle_class_types/java_simle_interface.java
@@ -1,24 +1,39 @@
-interface InterfaceExample1{
-    
-    public static int field = 0xA10F;
-    
-    public abstract void method();
-    
-}
+/*
+    Interface:
+    1. You cannot instantiate an interface
+    2. An interface does not contain any constructors.
+    3. All of the methods in an interface are abstract.
+    4. An interface cannot contain instance fields. The 
+       only fields that can appear in an interface must be declared both static and final.
+       
+    5. An interface is not extended by a class; it is implemented by a class.
+    6. An interface can extend multiple interfaces.
+*/
 
 interface InterfaceExample3{
     
-    String field3 = "InterfaceExample3";
+    /* all field implicitly are static and final */
+    /* static final */    String field3 = "InterfaceExample3";
     
-    void method();
+    /* all methods implicitly are  public and abstract */
+    /* public abstract */ void method();
     
 }
 
-interface InterfaceExample2 extends InterfaceExample3{
+interface InterfaceExample1{
     
-    public final int finalField = 1000;
+    public static /* final */ Demo demo = new Demo();
+    public static /* final */ int field = 0xA10F;
     
-    public abstract void method();
+    /*public*/ abstract void method();
+    
+}
+
+interface InterfaceExample2 extends InterfaceExample3 /* ,InterfaceExample2 ... */{
+    
+    public /* static */ final int finalField = 1000;
+    
+    public /* abstract */ void method();
     
     default void defaultMethod(String msg){
         System.out.println(msg);
@@ -27,4 +42,8 @@ interface InterfaceExample2 extends InterfaceExample3{
 
 class SimpleClass implements InterfaceExample1, InterfaceExample2{
     
+    @Override
+    public void method() {
+        
+    }
 }
