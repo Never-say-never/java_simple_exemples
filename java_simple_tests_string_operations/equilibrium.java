39eb1aa5164349a0382be6d12e0fe22e866229d0
Valeriy <fokin0210@gmail.com>
2015-02-10 23:12:50 +0100

    Create equilibrium.java

---
+++ java_simple_tests_string_operations/equilibrium.java
@@ -0,0 +1,23 @@
+private static int equilibrium(int[] A){
+        if(A == null || A.length <= 1){
+            return -1;
+        }
+        
+        long generallSumm = 0;
+        int length = A.length;
+        for(int ix = 0; ix < length; ++ix){
+            generallSumm += A[ix];
+        }
+        
+        long left = 0;
+        for(int ix = 1; ix < length; ++ix){
+            left += A[ix - 1];
+            long localRight = generallSumm - left - A[ix];
+            
+            if((localRight + left) == 0){
+                return ix;
+            }
+        }
+        
+        return -1;
+    }
